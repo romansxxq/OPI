31e819a5b3d65d5a06a745f18178a861776f6a33
romatvX_ <asssfsa010@gmail.com>
2026-03-06 00:07:14 +0200

    pr24: create data models in models.py

--- DjangoPracticWork/django_testproject/blog/models.py
+++ DjangoPracticWork/django_testproject/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from dataclasses import dataclass
 # Create your models here.
+@dataclass
+class User:
+    first_name: str
+    last_name: str
+    description: str
+
+@dataclass
+class Media:
+    title: str
+    description: str
+    rating: int
+    studio_name: str
