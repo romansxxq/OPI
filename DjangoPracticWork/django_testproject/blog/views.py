d1aa5d25a85625a97d76879b8f625d6371d9661b
romatvX_ <asssfsa010@gmail.com>
2026-03-06 00:07:48 +0200

    pr24: update home page to display user and media information; remove unused files

--- DjangoPracticWork/django_testproject/blog/views.py
+++ DjangoPracticWork/django_testproject/blog/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-
+from .models import User
+from .models import Media
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    user = User(first_name='Роман', last_name='Матвійчук', description='Student')
+    media = Media(title='Зелений слоник', description='Сімейний фільм', rating=100, studio_name='SuperPuper')
+    context = {
+        'user': user,
+        'media': media
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
