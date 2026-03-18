7f0a98a36a866f99c19d66013d6d0a121897770a
romatvX_ <146443792+romansxxq@users.noreply.github.com>
2026-03-18 14:31:44 +0200

    pr25: add media detail URL pattern to urlpatterns

--- DjangoPracticWork/django_testproject/blog/urls.py
+++ DjangoPracticWork/django_testproject/blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('media/<int:index>/', views.media_detail, name='media_detail'),
 ]
